15698849e111f23f9cb5082826ed0703bf4c396f
nsyawali12 <nsyawali12@gmail.com>
2021-05-19 13:57:52 +0700

    scrapping a web

--- simple/main.py
+++ simple/main.py
@@ -1,14 +1,22 @@
 import schedule
 import time
+import requests
 
 def job():
     print("Learning Scheduler...")
 def coding():
     print("Programming time ...")
+    
+def playing():
+    print("Playing time....")   
+    
 # creating time schedule example
 
 schedule.every(10).seconds.do(job)
 schedule.every(20).seconds.do(coding)
+
+## creating time schedule in everytime example
+schedule.every().day.at("11:40").do(playing)
 
 while True:
     schedule.run_pending()
